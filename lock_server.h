c5569621b9dc5da0881123508b4ad1f9501460aa
lionel <lixycm@gmail.com>
2017-12-19 20:51:02 +0800

    commit code

---
+++ lock_server.h
@@ -0,0 +1,35 @@
+// this is the lock server
+// the lock client has a similar interface
+
+#ifndef lock_server_h
+#define lock_server_h
+
+#include <string>
+#include "lock_protocol.h"
+#include "lock_client.h"
+#include "rpc.h"
+
+class lock_server {
+
+ protected:
+  int nacquire;
+  pthread_mutex_t smutex;
+  pthread_cond_t scond;
+  std::map<lock_protocol::lockid_t, bool> locks;
+
+ public:
+  lock_server();
+  ~lock_server() {};
+  lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
+};
+
+#endif 
+
+
+
+
+
+
+
